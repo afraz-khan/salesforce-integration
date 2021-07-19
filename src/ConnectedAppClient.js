6b84219fcd8d65789fa3c0ce087c03318bb6b52c
Afraz Khan <ah.afrazkhan@gmail.com>
2021-07-19 14:38:54 +0500

    added formatting.

--- src/ConnectedAppClient.js
+++ src/ConnectedAppClient.js
@@ -1,23 +1,18 @@
-const jsforce = require('jsforce');
-const {getToken} = require('sf-jwt-token');
-const fs = require('fs');
+const jsforce = require('jsforce')
+const { getToken } = require('sf-jwt-token')
 
 module.exports.ConnectedAppClient = async () => {
-    const conn = new jsforce.Connection();
-    const privateKey = fs.readFileSync('./server.key').toString('utf8');
+  const conn = new jsforce.Connection()
+  const jwtTokenResponse = await getToken({
+    iss: process.env.CLIENT_ID,
+    sub: process.env.USERNAME,
+    aud: process.env.LOGIN_URL,
+    privateKey: process.env.PRIVATE_KEY,
+  })
 
-    const jwtTokenResponse = await getToken(
-        {
-            iss: '3MVG9fe4g9fhX0E7T9UDCEjGUAP2mCUyVuoSgjELc0yg1sAcjP_NZ16ecPDVsW04_MkXtWuJiLKo4j9rHhP9b',
-            sub: 'me@afrazkhan.com',
-            aud: 'https://login.salesforce.com',
-            privateKey: privateKey,
-        }
-    );
-    conn.initialize({
-        instanceUrl: jwtTokenResponse.instance_url,
-        accessToken: jwtTokenResponse.access_token
-    })
-
-    return conn;
-};
+  conn.initialize({
+    instanceUrl: jwtTokenResponse.instance_url,
+    accessToken: jwtTokenResponse.access_token,
+  })
+  return conn
+}
--- src/queries.js
+++ src/queries.js
@@ -1,27 +1,33 @@
-const {ConnectedAppClient} = require('./ConnectedAppClient');
+const { ConnectedAppClient } = require('./ConnectedAppClient')
 
-var connection = null;
-ConnectedAppClient().then(conn => {
-    connection = conn;
-}).catch( err =>{
-    console.error(err);
-    throw err;
-});
+var connection = null
+ConnectedAppClient()
+  .then((conn) => {
+    connection = conn
+  })
+  .catch((err) => {
+    console.error(err)
+    throw err
+  })
 
 async function getProductItems() {
-    const productItems = await connection.query('SELECT ProductName, ProductItemNumber FROM ProductItem');
-    return productItems;
+  const productItems = await connection.query(
+    'SELECT ProductName, ProductItemNumber FROM ProductItem',
+  )
+  return productItems
 }
 
 async function getProductQuantity(productItemNum) {
-    const quantity = await connection.query(`SELECT QuantityOnHand FROM ProductItem WHERE ProductItemNumber='${productItemNum}'`);
-    if(quantity.totalSize === 0){
-        throw new Error('Product Item not found.');
-    }
-    return quantity;
+  const quantity = await connection.query(
+    `SELECT QuantityOnHand FROM ProductItem WHERE ProductItemNumber='${productItemNum}'`,
+  )
+  if (quantity.totalSize === 0) {
+    throw new Error('Product Item not found.')
+  }
+  return quantity
 }
 
 module.exports = {
-    getProductItems,
-    getProductQuantity,
+  getProductItems,
+  getProductQuantity,
 }
